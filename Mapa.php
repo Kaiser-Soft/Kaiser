343099690cf963306f97e5ff9f57794c43c3ed27
LeoRami99 <juanlov4321@gmail.com>
2020-05-12 20:55:57 -0500

    Error de mapa

--- Mapa.php
+++ Mapa.php
@@ -4,7 +4,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="es">
-
+	
     <head>
     <link rel="apple-touch-icon" sizes="57x57" href="favic/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="favic/apple-icon-60x60.png">
@@ -75,64 +75,10 @@
 						</a>
 				</form>
 			</div>
-		</nav>
-            <!--<script>
-                for (var i = 0; i <hola.length; i++) {
-                    document.writeln("["+hola[i]+"]\n");
-                }
-            </script>-->
+        </nav>
         <div id="map"></div>
-        <script>
-                // <!-- aqui empieza a pedir la ubicacion -->
-                if ("geolocation" in navigator){ //check Geolocation available 
-                //things to do
-            }else{
-                console.log("Geolocation not available!");
-            }
-
-            if ("geolocation" in navigator){ //check geolocation available 
-                //try to get user current location using getCurrentPosition() method
-                navigator.geolocation.getCurrentPosition(function(position){ 
-                        console.log("Found your location nLat : "+position.coords.latitude+" nLang :"+ position.coords.longitude);
-                    });
-            }else{
-                console.log("Browser doesn't support geolocation!");
-            }
-                
-            </script>
-        <script>
-                var customIcon=new L.icon({
-                    iconUrl:'images/iconpunto.png',
-                    iconSize:[38,50],
-                    iconAnchor:[22, 45]
-                });
-                var customIcon2=new L.icon({
-                    iconUrl:'images/iconpunto2.png',
-                    iconSize:[38,50],
-                    iconAnchor:[22, 45]
-                });
-                var map = L.map('map').
-                setView([5.30667 , -73.8144], 15);
-                
-                L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-                    maxZoom: 18
-                }).addTo(map);
-                L.control.scale().addTo(map);
-                //var marcador = L.marker([5.3163164, -73.8134494]).addTo('map').bindPopup('Hola').openPopup();
-                for (var i= 0; i < lati.length; i++) {
-                    
-                    L.marker([lati[i], longi[i]],{icon: customIcon}).addTo(map);
-                    //.bindPopup('Zona '+i).openPopup();
-                    
-                }
-                for (var i= 0; i < latiS.length; i++) {
-                    
-                    L.marker([latiS[i], longiS[i]],{icon: customIcon2}).addTo(map);
-                    //.bindPopup('Zona '+i).openPopup();
-                    
-                }
-        </script> 
+        <script src="ScriptsJS/Location.js"></script>
+        <script src="ScriptsJS/Mapa.js"></script> 
         </div>
         <!-- Footer -->
         <footer class="page-footer font-small">
